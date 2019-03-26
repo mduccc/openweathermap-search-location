620d01bff4c660c132bc712ec3a173ef1fb477dc
mduccc <duc2521997@gmail.com>
2019-03-27 00:25:46 +0700

    Commit

---
+++ app.js
@@ -0,0 +1,60 @@
+const express = require('express')
+const app = express()
+const fs = require('fs')
+const port = 3100
+
+app.listen(port, ()=> {
+    console.log('API running on port ' + port)
+})
+
+app.get('/location', async (req, res) => {
+    let key_word_raw = req.query.key_word.trim()
+    let key_word_trim = ''
+
+    let result = []
+    let code = 404
+
+    if (key_word_raw.trim().length == 0) {
+        res.json({
+            code: code,
+            key_word: key_word_trim,
+            result: result
+        })
+        res.end
+        return 1
+    }
+
+    // trim space between two word
+    let count_space = 0
+    for (let i = 0; i < key_word_raw.length; i++) {
+        if (key_word_raw[i] == ' ')
+            count_space++
+        else
+            count_space = 0
+
+        if (count_space < 2)
+            key_word_trim += key_word_raw[i]
+    }
+
+    console.log('Trim keyword: ', key_word_trim)
+
+    await fs.readFile('./city/city.list.json', (err, json) => {
+        let obj = JSON.parse(json)
+
+        for (let i=0; i<obj.length; i++) {
+            if (obj[i].name.toLowerCase().search(key_word_trim.toLowerCase()) > -1) 
+                result.push(obj[i])
+        }
+
+        if (result.length != 0) 
+            code = 200
+
+        res.json({
+            code: code,
+            key_word: key_word_trim,
+            result: result
+        })
+        res.end
+    })
+    
+})
